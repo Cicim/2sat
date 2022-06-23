f048ccbe6c27b7de776540967661f6b9f65ff004
Tiziano Finizzi <finizzi.1915610@studenti.uniroma1.it>
2022-06-23 10:47:58 +0200

    formatted implication graph

--- implication_graph.py
+++ implication_graph.py
@@ -1,4 +1,4 @@
-#TIZIANO FINIZZI
+# TIZIANO FINIZZI
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -6,35 +6,38 @@ options = {
     'node_color': 'lightblue',
     'node_size': 300,
     'width': 3,
-    'with_labels':True,
+    'with_labels': True,
 }
 
 
 class ImplicationGraph:
     def __init__(self, formula):
-    	self.g = nx.DiGraph()
-    	for i in range(0,len(formula)):
-    		#i-th clause
-    		c=formula[i]
-    		#create new graph nodes
-    		self.g.add_node(c[0])
-    		self.g.add_node(c[1])
-    		self.g.add_node(-c[0])
-    		self.g.add_node(-c[1])
-    		#add new edges to implication graph
-    		self.g.add_edge(c[0],-c[1])
-    		self.g.add_edge(-c[0],c[1])
-		
+        self.g = nx.DiGraph()
+        for i in range(0, len(formula)):
+            # i-th clause
+            c = formula[i]
+            # create new graph nodes
+            self.g.add_node(c[0])
+            self.g.add_node(c[1])
+            self.g.add_node(-c[0])
+            self.g.add_node(-c[1])
+            # add new edges to implication graph
+            self.g.add_edge(c[0], -c[1])
+            self.g.add_edge(-c[0], c[1])
+
     def literals(self):
-    	for l in self.g.nodes:
-        	yield l
+        for l in self.g.nodes:
+            yield l
+
     def successors(self, literal):
-      	for l in self.g.successors(literal):
-      		yield l
+        for l in self.g.successors(literal):
+            yield l
 
     def draw(self):
         nx.draw(self.g, **options)
         plt.show()
+
+
 '''
 	def draw_sccs(self, sccs):
         # Optional
@@ -46,16 +49,3 @@ class ImplicationGraph:
         representing the SCC.
         """
 '''
-    
-
-
-
-
-
-	
-
-
-
-	
-
-
